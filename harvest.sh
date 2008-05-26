51ab2436545ec4b7f0a444d03c14f9a530b8cf93
JSS <jss@krdwrd.org>
2008-05-26 14:19:56 +0000

    elaborate on auto-detection of input encoding; * try html meta tag (first match) * try file magic * fall back to utf-8

--- harvest.sh
+++ harvest.sh
@@ -6,20 +6,45 @@ export LANG=en_US.UTF-8
 
 for g in $@;
 do
-	echo $g
-	i=0
-	for url in `cat $g`;
-	do
-		cat=`basename $g .ggx`
-		ind=`echo $i | awk '{ printf("%03d", $1);}'`
-		b=`echo $url | sed 's/^\(.*\/\).*$/\1/' | sed 's/\//\\\\\//g' `
-		let i++
-		echo "$cat - $ind"
-		test -f $cat.$ind.html && continue
-		FN=$cat.$ind.html
-		wget --timeout=60 -o $cat.$ind.log --tries=2 -O $FN "$url" || continue
-		sed 's/<head>/<head><base href="'$b'"\/>/i' $FN > $FN.orig
-		iconv -t utf-8 $FN.orig -o $FN
-	done
+    echo $g
+    i=0
+    for url in `cat $g`;
+    do
+        cat=`basename $g .ggx`
+        ind=`echo $i | awk '{ printf("%03d", $1);}'`
+        b=`echo $url | sed 's/^\(.*\/\).*$/\1/' | sed 's/\//\\\\\//g' `
+        let i++
+        echo "$cat - $ind"
+        # skip if file exists
+        test -f $cat.$ind.html && continue
+        FN=$cat.$ind.html
+        # download
+        wget --timeout=60 -o $cat.$ind.log --tries=2 -O $FN "$url" 
+        if [[ $? -ne 0 ]]; then
+            rm $FN
+            echo "FAILED" >> $cat.$ind.log
+            echo "SKIPPED"
+            continue;
+        fi
+        # determine encoding
+        # 1. try content-type in html
+        ENC=`sed -n 's/^.*charset=\([0-9a-zA-Z_-]*\)".*$/\1/p' $FN | head -n1`
+        # 2. try detectin via file
+        test -n "$ENC" || ENC=`file -i $FN | sed -n 's/.*=\(.*$\)$/\1/p' `
+        test "$ENC" == "unknown" && ENC="utf-8"
+        # 3. fall back to utf8
+        test -n "$ENC" || ENC="utf-8"
+        ENC=`echo $ENC | sed 's/latin-\([0-8]\)/latin\1/'`
+        echo "Encoding: $ENC"
+        # convert to utf8
+        cp $FN $FN.orig
+        iconv -c -t utf8 -f $ENC $FN.orig -o $FN >> $cat.$ind.log
+        # replace previous charset definition
+        sed -i 's/\(^.*charset=\)[0-9a-zA-Z_-]*\(".*$\)/\1UTF-8\2/' $FN
+        sed -i 's/<meta http-equiv\ *=\ *"content-type"[^>]*>//i' $FN
+        # fix base url
+        sed -i 's/<head>/<head><base href="'$b'"\/><meta http-equiv="Content-Type" content="text\/html; charset=utf-8">/i' $FN 
+
+    done
 done
 
